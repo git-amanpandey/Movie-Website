fd0839ce01f8fa95d08e4cf3acf2cc3397e030c4
git-amanpandey <ap859151@gmail.com>
2023-08-31 19:36:40 +0530

    movie website in react done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import SingleMovie from "./SingleMovie";
+import Error from "./Error";
+ import "./App.css";
+import "./ErrorPage.css";
+import Default from "./Xyz/Default";
+import "./Xyz/Style.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route  exact path="/" element={<Default/>}/>
+      <Route exact path="/home" element={<Home />} />
+      <Route path="/movie/:id" element={<SingleMovie />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
   );
-}
+};
 
-export default App;
+export default App
---
+++ src/SingleMovie.js
@@ -0,0 +1,45 @@
+import { NavLink, useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+const SingleMovie = () => {
+
+  const { id } = useParams();
+  console.log(id);
+
+  const { isLoading , movie} = useFetch(`&i=${id}`);
+
+  if (isLoading) {
+    return (
+      <section className="movie-section ">
+        <div className="loading">Just Wait....✋</div>;
+      </section>
+    );
+  }
+
+  return (
+
+    <section className="movie-section">
+      <div className="movie-card">
+        <figure>
+          <img src={movie.Poster} alt="" />
+        </figure>
+        <div className="card-content">
+          <p className="title">{movie.Title}</p>
+          <p className=""></p>
+          <p className="card-text"><b>Released: </b>{movie.Released}</p>
+          <p className="card-text"><b>Genre: </b>{movie.Genre}</p>
+          <p className="card-text"><b>Language: </b>{movie.Language}</p>
+          {/* <p className="card-text"><b>Type:</b>{movie.Type}</p> */}
+          <p className="card-text"><b>Imdb-Rating: </b>{movie.imdbRating} / 10</p>
+          <p className="card-text"><b>Country: </b>{movie.Country}</p>
+          <p className="card-text"><b>Actors: </b>{movie.Actors}</p>
+          <NavLink to="/Home" className="back-btn">
+            Go Back
+          </NavLink>
+        </div>
+      </div>
+    </section>
+
+  );
+};
+
+export default SingleMovie;
